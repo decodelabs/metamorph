a6e5c3edaa1f734d45af010ea5ff53131dbf7cb1
Tom Wright <tom@inflatablecookie.com>
2025-11-04 13:04:53 +0000

    Improved doc comment headers
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- tests/AnalyzeFactoryCall.php
+++ tests/AnalyzeFactoryCall.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package Metamorph
- * @license http://opensource.org/licenses/MIT
+ * Metamorph
+ * @license https://opensource.org/licenses/MIT
  */
 
 declare(strict_types=1);
@@ -12,9 +12,10 @@ namespace DecodeLabs\Metamorph\Tests;
 use DecodeLabs\Metamorph;
 use Stringable;
 
-class AnalyzeFactoryCall {
-
-    public function render(): string|Stringable {
+class AnalyzeFactoryCall
+{
+    public function render(): string|Stringable
+    {
         return Metamorph::{'markdown.safe'}('Hello World');
     }
 }
